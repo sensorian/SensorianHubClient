6a6d9f43c09e323c83f743d46818b6f68cf746e5
Gunsmithy <gunsmithy@gmail.com>
2016-05-24 04:37:15 -0400

    Added the Broadcom driver to install script
    Install script was missing necessary Broadcom driver. It has been added
    to ensure a smoother install process.

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+#Update repos first just in case
+sudo apt-get update
+
 #Install other packages from the Raspbian Repository
 sudo apt-get -y install i2c-tools || { echo "Failed to install i2c-tools" && exit; }
 sudo apt-get -y install libi2c-dev || { echo "Failed to install libi2c-dev" && exit; }
@@ -16,14 +19,26 @@ sudo easy_install Pillow
 #Enter PythonSharedObjectSrc
 cd PythonSharedObjectSrc
 
+#Install Broadcom GPIO driver
+cd BCM2835
+tar zxvf bcm2835-1.50.tar.gz
+cd bcm2835-1.50
+./configure
+make
+sudo make check
+sudo make install
+
+#Compile the pressure sensor driver
 cd MPL3115A2
 make
 cd ..
 
+#Compile the Accelerometer driver
 cd FXOS8700CQR1
 make
 cd ..
 
+#Compile the Capacitive Touch Button driver
 cd CAP1203
 make
 cd ..
@@ -31,12 +46,12 @@ cd ..
 #Leave PythonSharedObjectSrc
 cd ..
 
-#Copy the files into where they are needed
+#Copy the driver files to where they are needed
 cp -p PythonSharedObjectSrc/MPL3115A2/libMPL.so ./libMPL.so
 
 cp -p PythonSharedObjectSrc/FXOS8700CQR1/libFXO.so ./libFXO.so
 
 cp -p PythonSharedObjectSrc/CAP1203/libCAP.so ./libCAP.so
 
-#At this point, everything should be installed.
+#At this point, everything should be installed and working after reboot
 echo "Sensorian Client dependencies should now be setup. Please reboot!"
